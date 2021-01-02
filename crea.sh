fd2b3c0a5f9e8621aa4b87c521eb548b708a0640
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-01-02 02:32:49 +0100

    fixed colors

--- crea.sh
+++ crea.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp /home/poulette/paper/server.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f
 echo -e "\e[32mServeur Créa mis à jour"
+sleep 2
+reset
--- survie.sh
+++ survie.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp /home/poulette/paper/server.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac
 echo -e "\e[32mServeur Survie mis à jour"
+sleep 2
+reset
