55ef136e21ec9eff0265a3c842b3fb9e2df76468
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-01-03 02:07:33 +0100

    spelling fix

--- plugins-update/plugins/TerraformGenerator.sh
+++ plugins-update/plugins/TerraformGenerator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /home/poulette/plugins/TerrafromGenerator.jar https://api.spiget.org/v2/resources/75132/download -q
+wget -O /home/poulette/plugins/TerraformGenerator.jar https://api.spiget.org/v2/resources/75132/download -q
 wget -O /home/poulette/plugins/Drycell.jar https://api.spiget.org/v2/resources/73386/download -q
 
 chmod +x /home/poulette/plugins/TerraformGenerator.jar
