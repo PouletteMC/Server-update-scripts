cbfe9a1dac11607c7fdc23bad08578d9e4298882
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-03 02:22:58 +0100

    Delete worldguard.sh

--- plugins-update/plugins/worldguard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -O /home/poulette/plugins/WorldGuard.jar https://dev.bukkit.org/projects/worldguard/files/latest -q
-chmod +x /home/poulette/plugins/WorldGuard.jar
-echo "WorldGuard mis à jour"
