984a5799c9716279b1ffc0ec192e854384f7f1c5
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-01-03 02:38:18 +0100

    fixed

--- plugins-update/waterfall.sh
+++ plugins-update/waterfall.sh
@@ -13,15 +13,15 @@ bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-script
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/ServerListPlus.sh)
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/TAB.sh)
 
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/MaSuiteChat.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/MaSuiteHomes.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/MaSuitePortals.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/MaSuiteTeleports.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/MaSuiteWarps.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/BungeeGuard.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/LuckPerms.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/Plan.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/MoveMeNow.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/ServerListPlus.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
-cp /home/poulette/plugins/TAB.jar /mnt/SSD/51301113-f872-4d08-8f6e-9d43a5336b41
+cp /home/poulette/plugins/MaSuiteCore.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteChat.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteHomes.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuitePortals.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteTeleports.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteWarps.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/BungeeGuard.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/LuckPerms.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/Plan.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MoveMeNow.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/ServerListPlus.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/TAB.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
