c603a787a83ff09e79006af23fc280566940e091
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-01-03 02:24:49 +0100

    deleted worldguard

--- plugins-update/paper.sh
+++ plugins-update/paper.sh
@@ -5,7 +5,6 @@ bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-script
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/LuckPerms.sh)
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/Plan.sh)
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/TerraformGenerator.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/WorldGuard.sh)
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/masuite/MaSuiteCore.sh)
 
 cp /home/poulette/plugins/BungeeGuard.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
@@ -37,11 +36,6 @@ cp /home/poulette/plugins/Drycell.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396
 cp /home/poulette/plugins/Drycell.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
 cp /home/poulette/plugins/Drycell.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
 cp /home/poulette/plugins/Drycell.jar /mnt/local_share/var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
-
-cp /home/poulette/plugins/WorldGuard.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/WorldGuard.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/WorldGuard.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/WorldGuard.jar /mnt/local_share/var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
 
 cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
 cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
