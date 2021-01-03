17607bc8ffa36b0acb8a06b839b7bd153f1b708d
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-01-03 01:36:12 +0100

    added waterfall

--- version-update/servers/waterfall.sh
+++ version-update/servers/waterfall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp /home/poulette/downloads/server.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41
+cp /home/poulette/downloads/waterfall.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41
--- version-update/update-version.sh
+++ version-update/update-version.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 wget -O /home/poulette/downloads/server.jar https://papermc.io/api/v1/paper/1.16.4/latest/download
+wget -O /home/poulette/downloads/waterfall.jar https://papermc.io/api/v1/waterfall/1.16/latest/download
 
 chmod +x /home/poulette/downloads/server.jar
+chmod +x /home/poulette/downloads/waterfall.jar
 
 if [[ $EUID -ne 0 ]];
     then
