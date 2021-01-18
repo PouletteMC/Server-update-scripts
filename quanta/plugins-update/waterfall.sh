8e72c9b670a45fc93aaf82b76df297f7f2a75ea3
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-18 20:28:25 +0100

    reorganized

--- quanta/plugins-update/waterfall.sh
+++ quanta/plugins-update/waterfall.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/masuite/MaSuiteCore.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/masuite/MaSuiteChat.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/masuite/MaSuiteHomes.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/masuite/MaSuitePortals.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/masuite/MaSuiteTeleports.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/masuite/MaSuiteWarps.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/BungeeGuard.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/LuckPerms.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/Plan.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/MoveMeNow.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/ServerListPlus.sh)
-bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins-update/plugins/TAB.sh)
-
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/masuite/MaSuiteCore.sh)
 cp /home/poulette/plugins/MaSuiteCore.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/masuite/MaSuiteChat.sh)
 cp /home/poulette/plugins/MaSuiteChat.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/masuite/MaSuiteHomes.sh)
 cp /home/poulette/plugins/MaSuiteHomes.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/masuite/MaSuitePortals.sh)
 cp /home/poulette/plugins/MaSuitePortals.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/masuite/MaSuiteTeleports.sh)
 cp /home/poulette/plugins/MaSuiteTeleports.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/masuite/MaSuiteWarps.sh)
 cp /home/poulette/plugins/MaSuiteWarps.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/BungeeGuard.sh)
 cp /home/poulette/plugins/BungeeGuard.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/LuckPerms.sh)
 cp /home/poulette/plugins/LuckPerms.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/Plan.sh)
 cp /home/poulette/plugins/Plan.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/MoveMeNow.sh)
 cp /home/poulette/plugins/MoveMeNow.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/ServerListPlus.sh)
 cp /home/poulette/plugins/ServerListPlus.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/plugins/TAB.sh)
 cp /home/poulette/plugins/TAB.jar /mnt/local_share/var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
