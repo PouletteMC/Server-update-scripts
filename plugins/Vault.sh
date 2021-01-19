900707a863f2e6365a03c39b4bdb10ad4712cec5
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-19 11:28:22 +0100

    added Vault

---
+++ plugins/Vault.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O /home/poulette/plugins/Vault.jar https://dev.bukkit.org/projects/vault/files/latest
+chmod +x /home/poulette/plugins/Vault.jar
+echo "Vault mis à jour"
--- quanta/plugins-update/paper.sh
+++ quanta/plugins-update/paper.sh
@@ -49,3 +49,11 @@ cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b64
 cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
 cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
 cp /home/poulette/plugins/MaSuiteCore.jar /mnt/local_share/var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+
+
+bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/Vault.sh)
+
+cp /home/poulette/plugins/Vault.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
+cp /home/poulette/plugins/Vault.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
+cp /home/poulette/plugins/Vault.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/Vault.jar /mnt/local_share/var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
