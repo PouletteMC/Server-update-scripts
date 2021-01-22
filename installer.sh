b6d632232bdee127961e71c0ad35c6dd68eba18a
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-22 17:10:34 +0100

    added installer

--- installer.sh
+++ installer.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wget -O /bin/update-servers
+wget -O /bin/update-servers https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/command-script.sh
+
+echo "La commande est installée"
