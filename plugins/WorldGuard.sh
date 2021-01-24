02a2d776c8742951589a4b20379abc9fcad5f267
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-25 00:54:59 +0100

    updated

--- plugins/WorldGuard.sh
+++ plugins/WorldGuard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /home/poulette/plugins/WorldGuard.jar https://dev.bukkit.org/projects/worldguard/files/latest --user-agent="Mozilla" http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4560400/
+wget -O /home/poulette/plugins/WorldGuard.jar https://dev.bukkit.org/projects/worldguard/files/latest -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "http://www.google.com" -q
 chmod +x /home/poulette/plugins/WorldGuard.jar
 echo "WorldGuard mis à jour"
