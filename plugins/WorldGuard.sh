2a869399865c56133545a80310791860d8db915f
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-25 13:54:22 +0100

    Fixed download

--- plugins/WorldGuard.sh
+++ plugins/WorldGuard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /home/poulette/plugins/WorldGuard.jar https://dev.bukkit.org/projects/worldguard/files/latest -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "http://www.google.com" -q
+wget -O /home/poulette/plugins/WorldGuard.jar https://dev.bukkit.org/projects/worldguard/files/latest -U "Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" -q
 chmod +x /home/poulette/plugins/WorldGuard.jar
 echo "WorldGuard mis à jour"
