89c5a4837992a0d46e7c1dbcc740b488598746b8
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-25 20:18:18 +0100

    updated user agent

--- plugins/Vault.sh
+++ plugins/Vault.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /home/poulette/plugins/Vault.jar https://dev.bukkit.org/projects/vault/files/latest -U "Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" -q
+wget -O /home/poulette/plugins/Vault.jar https://dev.bukkit.org/projects/vault/files/latest -U "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36" -q
 chmod +x /home/poulette/plugins/Vault.jar
 echo "Vault mis à jour"
