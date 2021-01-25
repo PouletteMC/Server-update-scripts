2503b139f2773a4d3515a767230537dcf56a6582
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-26 00:47:19 +0100

    Link change

--- plugins/Vault.sh
+++ plugins/Vault.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /home/poulette/plugins/Vault.jar https://dev.bukkit.org/projects/vault/files/latest -U "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36" -q
+wget -O /home/poulette/plugins/Vault.jar https://api.spiget.org/v2/resources/34315/download -q
 chmod +x /home/poulette/plugins/Vault.jar
 echo "Vault mis à jour"
