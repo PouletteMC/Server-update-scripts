4358e98fb6f5f562701ad3699e5af1c8fef1fd82
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-25 19:25:17 +0100

    updated download

--- plugins/Plan.sh
+++ plugins/Plan.sh
@@ -1,4 +1,4 @@
 #!/bin/#!/usr/bin/env bash
-wget -O /home/poulette/plugins/Plan.jar https://api.spiget.org/v2/resources/32536/download -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "http://www.google.com" -q
+wget -O /home/poulette/plugins/Plan.jar https://api.spiget.org/v2/resources/32536/download -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" -q
 chmod +x /home/poulette/plugins/Plan.jar
 echo "Plan mis à jour"
