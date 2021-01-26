37de813c30865e344e8a54d43c5fc3a27288c2b1
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-01-26 01:02:58 +0100

    removed undownloadable plugins

--- plugins/FAWE.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -O /home/poulette/plugins/FAWE.jar https://api.spiget.org/v2/resources/13932/download -q
-chmod +x /home/poulette/plugins/FAWE.jar
-echo "Fast Async WorldEdit mis à jour"
--- plugins/WorldGuard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -O /home/poulette/plugins/WorldGuard.jar https://www.curseforge.com/minecraft/bukkit-plugins/worldguard/download -U "Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" -q
-chmod +x /home/poulette/plugins/WorldGuard.jar
-echo "WorldGuard mis à jour"
