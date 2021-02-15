8fa189030634df46447a13b5b9625f73f153c534
Poulette <63922760+PouletteMC@users.noreply.github.com>
2021-02-15 14:59:29 +0100

    First steps

--- installer.sh
+++ installer.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 wget -O /bin/update-servers https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/command-script.sh
 
-echo "La commande est installée"
+read -p "Path to pterodactyl's servers:" pterodactyl_path
+
+servers=`ls $pterodactyl_path | wc -l`
+echo "il y a $servers serveurs"
+
+echo 'Voici les serveurs'
+tempfile=$(mktemp /tmp/servers.XXXXX)
+ls $pterodactyl_path > $tempfile
+echo Les données seront stockées dans $tempfile
+
+count=1
+
+for i in {1..$serveurs};
+do
+    sed -n "${count}p" < $tempfile
+    ((count=count+1))
+    echo $count
+done
+
+
+rm $tempfile
