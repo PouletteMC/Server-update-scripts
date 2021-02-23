a633cc7cc5cb18e9a7bcfad117c9d1771f520f50
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-02-23 14:42:37 +0100

    local only

--- asus/plugins-update/paper.sh
+++ asus/plugins-update/paper.sh
@@ -2,68 +2,41 @@
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/BungeeGuard.sh)
 
-cp /home/poulette/plugins/BungeeGuard.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/BungeeGuard.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/BungeeGuard.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/BungeeGuard.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/BungeeGuard.jar $PTEROLOBBY/plugins
 
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/FCP.sh)
 
-cp /home/poulette/plugins/FastChunkPregenerator.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/FastChunkPregenerator.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/FastChunkPregenerator.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/FastChunkPregenerator.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/FastChunkPregenerator.jar $PTEROLOBBY/plugins
 
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/LuckPerms.sh)
 
-cp /home/poulette/plugins/LuckPerms.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/LuckPerms.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/LuckPerms.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/LuckPerms.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/LuckPerms.jar $PTEROLOBBY/plugins
 
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/Plan.sh)
 
-cp /home/poulette/plugins/Plan.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/Plan.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/Plan.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/Plan.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/Plan.jar $PTEROLOBBY/plugins
 
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/TerraformGenerator.sh)
 
-cp /home/poulette/plugins/TerraformGenerator.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/TerraformGenerator.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/TerraformGenerator.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/TerraformGenerator.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/TerraformGenerator.jar $PTEROLOBBY/plugins
 
-cp /home/poulette/plugins/Drycell.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/Drycell.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/Drycell.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/Drycell.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/Drycell.jar $PTEROLOBBY/plugins
 
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuiteCore.sh)
 
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/MaSuiteCore.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROLOBBY/plugins
 
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/WorldGuard.sh)
 
-cp /home/poulette/plugins/WorldGuard.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/WorldGuard.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/WorldGuard.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/WorldGuard.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/WorldGuard.jar $PTEROLOBBY/plugins
 
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/Vault.sh)
 
-cp /home/poulette/plugins/Vault.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/Vault.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/Vault.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
-cp /home/poulette/plugins/Vault.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+cp /home/poulette/plugins/Vault.jar $PTEROLOBBY/plugins
--- asus/plugins-update/waterfall.sh
+++ asus/plugins-update/waterfall.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuiteCore.sh)
-cp /home/poulette/plugins/MaSuiteCore.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuiteChat.sh)
-cp /home/poulette/plugins/MaSuiteChat.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
-
+cp /home/poulette/plugins/MaSuiteChat.jar $PTEROPROXY/plugins
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuiteHomes.sh)
-cp /home/poulette/plugins/MaSuiteHomes.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteHomes.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuitePortals.sh)
-cp /home/poulette/plugins/MaSuitePortals.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuitePortals.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuiteTeleports.sh)
-cp /home/poulette/plugins/MaSuiteTeleports.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteTeleports.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuiteWarps.sh)
-cp /home/poulette/plugins/MaSuiteWarps.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MaSuiteWarps.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/BungeeGuard.sh)
-cp /home/poulette/plugins/BungeeGuard.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/BungeeGuard.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/LuckPerms.sh)
-cp /home/poulette/plugins/LuckPerms-Waterfall.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/LuckPerms-Waterfall.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/Plan.sh)
-cp /home/poulette/plugins/Plan.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/Plan.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/MoveMeNow.sh)
-cp /home/poulette/plugins/MoveMeNow.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/MoveMeNow.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/ServerListPlus.sh)
-cp /home/poulette/plugins/ServerListPlus.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/ServerListPlus.jar $PTEROPROXY/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/TAB.sh)
-cp /home/poulette/plugins/TAB.jar /var/lib/pterodactyl/volumes/51301113-f872-4d08-8f6e-9d43a5336b41/plugins
+cp /home/poulette/plugins/TAB.jar $PTEROPROXY/plugins
