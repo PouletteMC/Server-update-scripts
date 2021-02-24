ac53ae6290cc7ad8752bf464613a272f355ddba7
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-02-24 21:16:49 +0100

    added support for EV

--- quanta/plugins-update/paper.sh
+++ quanta/plugins-update/paper.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/BungeeGuard.sh)
-cp /home/poulette/plugins/BungeeGuard.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/BungeeGuard.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/BungeeGuard.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/BungeeGuard.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/BungeeGuard.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/BungeeGuard.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/FCP.sh)
-cp /home/poulette/plugins/FastChunkPregenerator.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/FastChunkPregenerator.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/FastChunkPregenerator.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/FastChunkPregenerator.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/FastChunkPregenerator.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/FastChunkPregenerator.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/LuckPerms.sh)
-cp /home/poulette/plugins/LuckPerms.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/LuckPerms.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/LuckPerms.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/LuckPerms.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/LuckPerms.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/LuckPerms.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/Plan.sh)
-cp /home/poulette/plugins/Plan.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/Plan.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/Plan.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/Plan.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/Plan.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/Plan.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/TerraformGenerator.sh)
-cp /home/poulette/plugins/TerraformGenerator.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/Drycell.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
+cp /home/poulette/plugins/TerraformGenerator.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/TerraformGenerator.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/TerraformGenerator.jar $PTEROCREA/plugins
+cp /home/poulette/plugins/Drycell.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/Drycell.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/Drycell.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/masuite/MaSuiteCore.sh)
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/WorldGuard.sh)
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/MaSuiteCore.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/MaSuiteCore.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/Vault.sh)
-cp /home/poulette/plugins/Vault.jar /mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/Vault.jar /mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/Vault.jar /mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/Vault.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/Vault.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/Vault.jar $PTEROCREA/plugins
 
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/plugins/Dynmap.sh)
-cp /home/poulette/plugins/Dynamp.jar /mnt/quanta/mnt/SSD/55c0ebc9-a763-4294-b760-e6b644396687/plugins
-cp /home/poulette/plugins/Dynamp.jar /mnt/quanta/mnt/SSD/526cb2cb-becd-4af8-a33b-fdb0636b05ac/plugins
-cp /home/poulette/plugins/Dynamp.jar /mnt/quanta/mnt/SSD/2d04f1e6-74c5-4215-88e8-4a02ba08741f/plugins
+cp /home/poulette/plugins/Dynamp.jar $PTEROSURVIE/plugins
+cp /home/poulette/plugins/Dynamp.jar $PTEROSANDBOX/plugins
+cp /home/poulette/plugins/Dynamp.jar $PTEROCREA/plugins
