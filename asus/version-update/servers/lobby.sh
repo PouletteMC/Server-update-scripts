744eccaa8b68ff4e6f240b806e0498d38169ed82
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-02-24 21:15:58 +0100

    moved jar download to this script

--- asus/version-update/servers/lobby.sh
+++ asus/version-update/servers/lobby.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cp /home/poulette/downloads/server.jar /var/lib/pterodactyl/volumes/c5b4cb41-0d23-484d-b9ad-4b07f9197177
+wget -O /home/poulette/downloads/server.jar https://papermc.io/api/v1/paper/1.16.5/latest/download -q
+chmod +x /home/poulette/downloads/server.jar
+
+cp /home/poulette/downloads/server.jar $PTEROLOBBY
 echo "Lobby a été mis à jour"
