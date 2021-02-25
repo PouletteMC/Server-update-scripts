fcba51a6d561fa6b175a94615427a78eabbad772
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2021-02-25 01:05:35 +0100

    removed unused code

--- quanta/plugins-update/update-plugins.sh
+++ quanta/plugins-update/update-plugins.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-if [[ $EUID -ne 0 ]];
-    then
-        echo "This command must be executed as root"
-        exit 1
-fi
-
 bash <(curl -s https://raw.githubusercontent.com/PouletteMC/Server-update-scripts/main/quanta/plugins-update/paper.sh)
 echo "Paper plugins have been updated"
